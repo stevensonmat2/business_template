85fbd9e8db92458c536fe2c763a3f5dbdc4dfaa6
matt <stevensonmat2@gmail.com>
2019-01-20 21:59:49 -0800

    update

--- business_main.js
+++ business_main.js
@@ -1,15 +1,5 @@
-// var myNav = document.getElementById("navbar");
-// window.onscroll = function () {
-//     "use strict";
-//     if (document.body.scrollTop >= 200 ) {
-//         myNav.classList.add("nav-colored");
-//         myNav.classList.remove("nav-transparent");
-//     }
-//     else {
-//         myNav.classList.add("nav-transparent");
-//         myNav.classList.remove("nav-colored");
-//     }
-// };
+
+// CHANGE NAVBAR COLOR ON SCROLL
 
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
@@ -20,3 +10,34 @@ $(window).scroll(function() {
         $(".navbar").removeClass("nav-colored");
     }
 });
+
+
+// $(document).ready(function(){
+//    $(".nav-item").click(function () {
+//      $("a.active").removeClass('active');
+//      $(this).addClass('active');
+//      var active_section = $(this).attr('href'); //get active section id
+//      $('html, body').animate({
+//      //and scroll to the section
+//      scrollTop: $(active_section).offset().top
+//      });
+//    });
+//
+//
+//   $(document).scroll(function () {
+//   //get document scroll position
+//     var position = $(document).scrollTop();
+//     //get header height
+//     var header = $('nav').outerHeight();
+//
+//     //check active section
+//     $('.section').each(function(i) {
+//         if($(this).position().top <= (position + header))
+//          {
+//               $("a.active").removeClass('active');
+//               $("a").eq(i).addClass('active');
+//          }
+//      });
+//   });
+//
+// });
